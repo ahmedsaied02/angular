1c11134e8e9140e421ea1ce318be0140e225e2fe
ahmed saied <ahmed.saiedd02@gmail.com>
2023-05-03 14:40:25 +0300

    responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faShoppingBag } from '@fortawesome/free-solid-svg-icons';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimes,faBars} from '@fortawesome/free-solid-svg-icons';
 import { faSearchLocation } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -13,8 +13,16 @@ export class HeaderComponent implements OnInit {
   constructor() { }
   faShoppingBag = faShoppingBag; 
   faTimes=faTimes;
+  faBars=faBars;
   faSearchLocation =faSearchLocation;
   ngOnInit(): void {
   }
+  navigation = "nonActive";
 
+  showMenu() {
+    this.navigation="Active";
+  }
+  hideMenu() {
+    this.navigation = "nonActive";
+  }
 }
